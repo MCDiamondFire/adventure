51e480c7035a18e4c07ae5e0c43bdc4d8f9aa92f
KabanFriends <45000995+kabanfriends@users.noreply.github.com>
2023-09-20 09:24:25 +0300

    DF tweaks (#1)
    * temp
    
    * Disable forced color code check
    
    * Revert
    
    * Remove failing tests
    
    * Check style fix

--- text-minimessage/src/main/java/net/kyori/adventure/text/minimessage/internal/parser/TokenParser.java
+++ text-minimessage/src/main/java/net/kyori/adventure/text/minimessage/internal/parser/TokenParser.java
@@ -159,6 +159,8 @@ public final class TokenParser {
     final int length = message.length();
     for (int i = 0; i < length; i++) {
       final int codePoint = message.codePointAt(i);
+      // DiamondFire start
+      /*
       if (!lenient && codePoint == '§' && i + 1 < length) {
         final int nextChar = Character.toLowerCase(message.codePointAt(i + 1));
         // Only throw an exception if the next character is actually going to make a legacy color code
@@ -175,6 +177,8 @@ public final class TokenParser {
           );
         }
       }
+      */
+      // DiamondFire end
 
       if (!Character.isBmpCodePoint(codePoint)) {
         i++;
